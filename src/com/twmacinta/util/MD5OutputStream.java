77ccf94955aead61df27bfa7b865081654cbb813
asdf <asdf@asdf.com>
2012-04-24 16:55:47 +1200

    Beautify the code.

--- src/com/twmacinta/util/MD5OutputStream.java
+++ src/com/twmacinta/util/MD5OutputStream.java
@@ -37,80 +37,80 @@ import java.io.*;
  **/
 
 public class MD5OutputStream extends FilterOutputStream {
-    /**
-     * MD5 context
-     */
-    private MD5	md5;
+	/**
+	 * MD5 context
+	 */
+	private MD5	md5;
 
-    /**
-     * Creates MD5OutputStream
-     * @param out	The output stream
-     */
+	/**
+	 * Creates MD5OutputStream
+	 * @param out	The output stream
+	 */
 
-    public MD5OutputStream (OutputStream out) {
-	super(out);
+	public MD5OutputStream (OutputStream out) {
+		super(out);
 
-	md5 = new MD5();
-    }
+		md5 = new MD5();
+	}
 
-    /**
-     * Writes a byte. 
-     *
-     * @see java.io.FilterOutputStream
-     */
+	/**
+	 * Writes a byte. 
+	 *
+	 * @see java.io.FilterOutputStream
+	 */
 
-    public void write (int b) throws IOException {
-	out.write(b);
-	md5.Update((byte) b);
-    }
+	public void write (int b) throws IOException {
+		out.write(b);
+		md5.Update((byte) b);
+	}
 
-    /**
-     * Writes a sub array of bytes.
-     *
-     * @see java.io.FilterOutputStream
-     */
+	/**
+	 * Writes a sub array of bytes.
+	 *
+	 * @see java.io.FilterOutputStream
+	 */
 
-    public void write (byte b[], int off, int len) throws IOException {
-	out.write(b, off, len);
-	md5.Update(b, off, len);
-    }
+	public void write (byte b[], int off, int len) throws IOException {
+		out.write(b, off, len);
+		md5.Update(b, off, len);
+	}
 
-    /**
-     * Returns array of bytes representing hash of the stream as finalized
-     * for the current state.
-     * @see MD5#Final
-     */
+	/**
+	 * Returns array of bytes representing hash of the stream as finalized
+	 * for the current state.
+	 * @see MD5#Final
+	 */
 
-    public byte[] hash () {
-	return md5.Final();
-    }
+	public byte[] hash () {
+		return md5.Final();
+	}
 
-  public MD5 getMD5() {
-    return md5;
-  }
+	public MD5 getMD5() {
+		return md5;
+	}
 
-  /**
-   * This method is here for testing purposes only - do not rely
-   * on it being here.
-   **/
-  public static void main(String[] arg) {
-    try {
-      MD5OutputStream out = new MD5OutputStream(new com.twmacinta.io.NullOutputStream());
-      InputStream in = new BufferedInputStream(new FileInputStream(arg[0]));
-      byte[] buf = new byte[65536];
-      int num_read;
-      long total_read = 0;
-      while ((num_read = in.read(buf)) != -1) {
-	total_read += num_read;
-	out.write(buf, 0, num_read);
-      }
-      System.out.println(MD5.asHex(out.hash())+"  "+arg[0]);
-      in.close();
-      out.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+	/**
+	 * This method is here for testing purposes only - do not rely
+	 * on it being here.
+	 **/
+	public static void main(String[] arg) {
+		try {
+			MD5OutputStream out = new MD5OutputStream(new com.twmacinta.io.NullOutputStream());
+			InputStream in = new BufferedInputStream(new FileInputStream(arg[0]));
+			byte[] buf = new byte[65536];
+			int num_read;
+			long total_read = 0;
+			while ((num_read = in.read(buf)) != -1) {
+				total_read += num_read;
+				out.write(buf, 0, num_read);
+			}
+			System.out.println(MD5.asHex(out.hash())+"  "+arg[0]);
+			in.close();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 
 }

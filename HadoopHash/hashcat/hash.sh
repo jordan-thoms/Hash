17430bd890ad1d98943b84f9517eac7ad9c29a34
Jordan Thoms <jordan@thoms.net.nz>
2012-05-04 15:29:07 +1200

    Allow hash.sh to be called from any directory and fix the checked statistic output

--- HadoopHash/hashcat/hash.sh
+++ HadoopHash/hashcat/hash.sh
@@ -3,21 +3,22 @@ hash=$1
 pattern=$2
 arch=`uname -m`
 ID=$RANDOM
-
-mpargs="-1 ?l?d?s?u $2 -o hashes$ID"
+MYDIR="$(dirname "$(readlink -f "$0")")"
+cd $MYDIR
+mpargs="-1 ?l?d?u $2 -o hashes$ID"
 hcargs="-n 1 hash$ID.hash hashes$ID -o results$ID"
 touch results$ID
 echo $hash > hash$ID.hash
 mkfifo hashes$ID # This is a pipe for the password candidates
 if [ $arch = "x86_64" ]; then
     ./mp64.bin $mpargs &
-    (./hashcat-cli64.bin $hcargs) > hashoutput$ID
+    (./hashcat-cli64.bin $hcargs) > hashoutput$ID   
 else
     ./mp32.bin $mpargs &
     (./hashcat-cli32.bin $hcargs) > hashoutput$ID
 fi
 rm hashes$ID
-checked=`cat hashoutput$ID | grep 'Progress..' | tail -n 1 | cut -d ' ' -f 2 | cut -d '/' -f 2`
+checked=`cat hashoutput$ID | grep 'Progress..' | cut -d ' ' -f 2 | cut -d '/' -f 2 | tr '\n' '+' | sed -e "s/+$/\n/g" |bc`
 rm hashoutput$ID
 echo "Checked:$checked"
 password=`cat results$ID | cut -d ':' -f 2`

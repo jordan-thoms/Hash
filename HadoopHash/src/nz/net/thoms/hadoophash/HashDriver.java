806b80909deaae38e6d3b30697ae9571b3f676f8
Jordan Thoms <jordan@thoms.net.nz>
2012-05-05 22:34:24 +1200

    - Skip remaining work when the match is found - Remove hardcoded values - You pass the max length, it checks values up to that

--- HadoopHash/src/nz/net/thoms/hadoophash/HashDriver.java
+++ HadoopHash/src/nz/net/thoms/hadoophash/HashDriver.java
@@ -1,12 +1,18 @@
 package nz.net.thoms.hadoophash;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
@@ -16,9 +22,34 @@ import org.apache.hadoop.mapred.RunningJob;
 import org.apache.hadoop.mapred.TextOutputFormat;
 
 public class HashDriver {
-
+	public static JobClient client = new JobClient();
 	public static void main(String[] args) throws URISyntaxException {
-		JobClient client = new JobClient();
+		String hash =  args[0];
+		int max_length = Integer.parseInt(args[1]);
+
+		for (int i=1; i<=max_length; i++) {
+			System.out.println("Running for length " + i);
+			String output = run(hash, i);
+			if (output != null) {
+				System.out.println(output);
+				break;
+			}
+		}
+
+	}
+
+	private static String run(String hash, int length) throws URISyntaxException {
+		if (length < 4) {
+			// As an optimization, just run it locally. (it takes about 20 seconds in overhead for the hadoop jobs).
+			HashExecutor executor = new SimpleHashExecutor();
+			HashResult result = executor.executeHashes(hash, "", length);
+			if (result.password != null) {
+				return hash + "   " + result.password;
+			} else {
+				return null;
+			}
+		}
+		System.out.println("Setting up job");
 		JobConf conf = new JobConf(nz.net.thoms.hadoophash.HashDriver.class);
 
 		// TODO: specify output types
@@ -31,22 +62,38 @@ public class HashDriver {
 		conf.setMapperClass(nz.net.thoms.hadoophash.HashMapper.class);
 		conf.setNumTasksToExecutePerJvm(1000);
 		conf.setOutputFormat(TextOutputFormat.class);
-		
-	    DistributedCache.addCacheArchive(new URI("/app/hashcat.zip#hashcat"), conf);
-	    DistributedCache.createSymlink(conf);
+
+		DistributedCache.addCacheArchive(new URI("/app/hashcat.zip#hashcat"), conf);
+		DistributedCache.createSymlink(conf);
 		DateFormat df = new SimpleDateFormat("mm_hh_ss");
-
-		FileOutputFormat.setOutputPath(conf, new Path("output" + df.format(new Date())));
-		conf.set("hash", "594f803b380a41396ed63dca39503542");
-		conf.setInt("password_length", 5);
+		Path outputPath = new Path("/app/output" + df.format(new Date()));
+		FileOutputFormat.setOutputPath(conf, outputPath);
+		conf.set("hash",  hash);
+		conf.setInt("password_length", length);
 		conf.setInt("tasks", 300);
-
 		client.setConf(conf);
+		System.out.println("Running job on hadoop");
 		try {
 			RunningJob job = JobClient.runJob(conf);
+			if(job.getCounters().findCounter("Hashes", "Found").getValue() == 0) {
+				System.out.println("Finished job, not found");
+				return null;
+			} else {
+				System.out.println("Finished job, found");
+				Configuration config = new Configuration();
+				FileSystem hdfs = FileSystem.get(config);
+				Path path = new Path(outputPath.toString() + "/part-00000");
+				if(hdfs.exists(path)) {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(hdfs.open(path)));
+					System.out.println("Found file at " + path.toString());
+					return reader.readLine();
+				} else {
+					System.out.println("Coult not find file");
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
-
 }
--- HadoopHash/src/nz/net/thoms/hadoophash/HashMapper.java
+++ HadoopHash/src/nz/net/thoms/hadoophash/HashMapper.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.mapred.Counters.Counter;
+import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.JobID;
 import org.apache.hadoop.mapred.JobTracker;
@@ -16,42 +20,51 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.RunningJob;
+import org.eclipse.core.runtime.jobs.Job;
 
 import com.twmacinta.util.MD5;
 
 public class HashMapper extends MapReduceBase implements Mapper<WritableComparable, HashValue, Text, Text> {
 	private JobConf job;
+	private Counter foundCounter;
 	@Override
 	public void configure(JobConf job) {
 		this.job = job;
+		try {
+			JobClient client = new JobClient(job);
+			RunningJob parentJob;
+			parentJob = client.getJob(JobID.forName( job.get("mapred.job.id") ));
+			foundCounter = parentJob.getCounters().findCounter("Hashes", "Found");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	private final HashExecutor hashExecutor = new HashcatHashExecutor();
-	
+
 	@Override
 	public void map(WritableComparable key, HashValue value,
 			OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-		String hash = value.getHash();
-		String prefix = value.getPrefix();
-		int length = value.getLength();
-		System.out.println("Mapping key: " + key + " hash: " + hash + " prefix: " + prefix + " length: " + length);
-		HashResult result = hashExecutor.executeHashes(hash, prefix, length);
-		if (result.password != null) {
-			System.out.println("Password is " + result.password);
-			output.collect(new Text(hash), new Text(result.password));
+		if (foundCounter == null || foundCounter.getValue() == 0) {
+			String hash = value.getHash();
+			String prefix = value.getPrefix();
+			int length = value.getLength();
+			System.out.println("Mapping key: " + key + " hash: " + hash + " prefix: " + prefix + " length: " + length);
+			HashResult result = hashExecutor.executeHashes(hash, prefix, length);
+			if (result.password != null) {
+				System.out.println("Password is " + result.password);
+				output.collect(new Text(hash), new Text(result.password));
+				reporter.getCounter("Hashes", "Found").increment(1);
+			}
+			reporter.getCounter("Hashes", "Checked").increment(result.checked);
 		}
-        reporter.getCounter("Hashes", "Checked").increment(result.checked);
 	}
 
-//	JobID jobId = JobID.forName(job.get("xpatterns.hadoop.content.job.id"));
-//	JobTracker tracker;
-//	try {
-//		tracker = JobTracker.startTracker(job);
-//		tracker.killJob(jobId);
-//		tracker.stopTracker();
-//	} catch (InterruptedException e) {
-//		// TODO Auto-generated catch block
-//		e.printStackTrace();
-//	}
 
 }
